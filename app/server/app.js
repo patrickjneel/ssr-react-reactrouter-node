c9110c930cb8424abea5ec42d3c457578b8f52d8
Pat Neel <patrick.neel@colorado.edu>
2018-05-30 15:24:13 -0600

    further set up

--- app/server/app.js
+++ app/server/app.js
@@ -1,1 +1,16 @@
-app.js
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
+
+import router from './router';
+
+const app = express();
+
+const assests = express.static(path.join(__dirname, '../'));
+
+app.use(cors());
+app.use(assets);
+
+app.get('*', router);
+
+export default app;
